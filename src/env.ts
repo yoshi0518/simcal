60dab2d1a1910d5f32c59ebc02357fb0e0c40dc9
yoshi0518 <yoshihumi.n.0518@gmail.com>
2025-09-20 11:06:21 +0900

    GithubActions追加、不要機能削除

--- src/env.ts
+++ src/env.ts
@@ -2,27 +2,11 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   DEBUG: z.string().transform((value) => value === 'true'),
-  NEON_DATABASE_URL: z.string(),
-  RESEND_API_KEY: z.string(),
-  RESEND_FROM: z.string(),
-  RESEND_ADMIN: z.string(),
-  MICROCMS_SERVICE_DOMAIN: z.string(),
-  MICROCMS_API_KEY: z.string(),
-  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z.string(),
-  RECAPTCHA_SECRET_KEY: z.string(),
   NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID: z.string(),
 });
 
 const parsedEnv = envSchema.safeParse({
   DEBUG: process.env.DEBUG,
-  NEON_DATABASE_URL: process.env.NEON_DATABASE_URL,
-  RESEND_API_KEY: process.env.RESEND_API_KEY,
-  RESEND_FROM: process.env.RESEND_FROM,
-  RESEND_ADMIN: process.env.RESEND_ADMIN,
-  MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
-  MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
-  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
-  RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
   NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID,
 });
 
--- src/types/recaptcha.ts
+++
@@ -1,9 +0,0 @@
-// https://developers.google.com/recaptcha/docs/v3?hl=ja#site_verify_response
-export type RecaptchaResultType = {
-  'success': boolean;
-  'score': number;
-  'action': string;
-  'challenge_ts': string;
-  'hostname': string;
-  'error-codes'?: string[];
-};
